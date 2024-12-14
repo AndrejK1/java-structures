7c0ab58afed4aed9641480c1a7fa9a84dcf203c1
Andrii Kononenko <andrju3952@gmail.com>
2024-12-15 00:30:35 +0200

    Clean

--- src/main/java/aoc/aoc2024/AOC10HoofIt.java
+++ src/main/java/aoc/aoc2024/AOC10HoofIt.java
@@ -5,7 +5,6 @@ import aoc.AOCTask;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static aoc.AOCMatrixUtils.DIRECTIONS;
 import static aoc.AOCMatrixUtils.calcNewPosition;
